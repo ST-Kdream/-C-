fd3ee46c2509acc2a60441edc5c24db13c71753e
朱孜轩 <3910684824@qq.com>
2025-09-29 20:43:05 +0800

    新增注释

--- 猜数字（C++）/猜数字（C++）.cpp
+++ 猜数字（C++）/猜数字（C++）.cpp
@@ -3,31 +3,31 @@
 #include <string>
 #include <thread>
 #include <chrono>
-#include "number_check.h"
+#include "number_check.h"    //函数头文件，用于输入验证
 
 using namespace std;
-
+//主函数
 int main() {
-
+	//变量定义
 	int max_num = 0;
 	int chance = 0;
 	int difficulty = 0;
-	string difficulty_name = "暂无";
+	string difficulty_name = "暂无";  
 
 
-
+	//游戏介绍和难度选择
 	cout << "欢迎来到猜数字游戏！" << endl;
 	this_thread::sleep_for(chrono::seconds(1));
 	cout << "接下来进行难度选择\n1为\t简单\n2为\t普通\n3为\t困难\n4为\t噩梦\n5为\t地狱\n6为\t自定义" << endl;
 	
 	get_valid_int(difficulty, 1, 6, "请输入难度选择(输入整数1到6）: ");
 
-	
+	//难度设置
 	int difficulty_set[2][6] = { {100, 100, 1000, 5000, 50000, 0},
 							     { 10 , 6 , 12 ,  15 ,  20  ,  0} };
 	string difficulty_names[6] = { "简单","普通","困难","噩梦","地狱","自定义" };
 	
-	
+	//根据选择设置参数
 	max_num = difficulty_set[0][difficulty - 1];
 	chance = difficulty_set[1][difficulty - 1];
 	difficulty_name = difficulty_names[difficulty - 1];
@@ -41,7 +41,7 @@ if (difficulty == 6) {
 	
 		
 
-
+//游戏开始倒计时
 	cout << "你选择的难度为：" << difficulty_name << "，答案在1到" << max_num << "之间，你有" << chance << "次机会猜中答案。" << endl;
 	cout << "游戏将在3秒后开始，请做好准备！" << endl;
 	int enter_time = 3;
@@ -55,17 +55,17 @@ if (difficulty == 6) {
 
 
 
-
+	//随机数生成器
 	random_device rd;
 	mt19937 gen(rd());
 	uniform_int_distribution<>int_dis(1, max_num);
-
+	//游戏变量
 	int answer = int_dis(gen);
 	int guess = 0;
 	int attempt = 0;
 
 
-
+	//游戏主循环
 	cout << "请输入一个1到" << max_num << "之间的整数：" << endl;
 
 
